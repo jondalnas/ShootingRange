ece5ba21a72f4ed2383f5f00948be87d6e6dd332
lenehc <lene.hojberg.christensen@gmail.com>
2021-01-05 16:43:40 +0100

    Added numpad and progress bar

--- ShootingRange/Form1.Designer.cs
+++ ShootingRange/Form1.Designer.cs
@@ -25,13 +25,228 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.distBtn = new System.Windows.Forms.Button();
+			this.btn7 = new System.Windows.Forms.Button();
+			this.btn8 = new System.Windows.Forms.Button();
+			this.btn9 = new System.Windows.Forms.Button();
+			this.btn4 = new System.Windows.Forms.Button();
+			this.btn5 = new System.Windows.Forms.Button();
+			this.btn6 = new System.Windows.Forms.Button();
+			this.btn3 = new System.Windows.Forms.Button();
+			this.btn2 = new System.Windows.Forms.Button();
+			this.btn1 = new System.Windows.Forms.Button();
+			this.btn0 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.delBtn = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(12, 415);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(776, 23);
+			this.progressBar1.TabIndex = 0;
+			// 
+			// distBtn
+			// 
+			this.distBtn.Location = new System.Drawing.Point(25, 317);
+			this.distBtn.Name = "distBtn";
+			this.distBtn.Size = new System.Drawing.Size(162, 23);
+			this.distBtn.TabIndex = 2;
+			this.distBtn.Text = "Go to distance";
+			this.distBtn.UseVisualStyleBackColor = true;
+			this.distBtn.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// btn7
+			// 
+			this.btn7.Location = new System.Drawing.Point(25, 50);
+			this.btn7.Name = "btn7";
+			this.btn7.Size = new System.Drawing.Size(50, 50);
+			this.btn7.TabIndex = 4;
+			this.btn7.Text = "7";
+			this.btn7.UseVisualStyleBackColor = true;
+			this.btn7.Click += new System.EventHandler(this.btn7_Click);
+			// 
+			// btn8
+			// 
+			this.btn8.Location = new System.Drawing.Point(81, 50);
+			this.btn8.Name = "btn8";
+			this.btn8.Size = new System.Drawing.Size(50, 50);
+			this.btn8.TabIndex = 5;
+			this.btn8.Text = "8";
+			this.btn8.UseVisualStyleBackColor = true;
+			this.btn8.Click += new System.EventHandler(this.btn8_Click);
+			// 
+			// btn9
+			// 
+			this.btn9.Location = new System.Drawing.Point(137, 50);
+			this.btn9.Name = "btn9";
+			this.btn9.Size = new System.Drawing.Size(50, 50);
+			this.btn9.TabIndex = 6;
+			this.btn9.Text = "9";
+			this.btn9.UseVisualStyleBackColor = true;
+			this.btn9.Click += new System.EventHandler(this.btn9_Click);
+			// 
+			// btn4
+			// 
+			this.btn4.Location = new System.Drawing.Point(25, 106);
+			this.btn4.Name = "btn4";
+			this.btn4.Size = new System.Drawing.Size(50, 50);
+			this.btn4.TabIndex = 7;
+			this.btn4.Text = "4";
+			this.btn4.UseVisualStyleBackColor = true;
+			this.btn4.Click += new System.EventHandler(this.btn4_Click);
+			// 
+			// btn5
+			// 
+			this.btn5.Location = new System.Drawing.Point(81, 106);
+			this.btn5.Name = "btn5";
+			this.btn5.Size = new System.Drawing.Size(50, 50);
+			this.btn5.TabIndex = 8;
+			this.btn5.Text = "5";
+			this.btn5.UseVisualStyleBackColor = true;
+			this.btn5.Click += new System.EventHandler(this.btn5_Click);
+			// 
+			// btn6
+			// 
+			this.btn6.Location = new System.Drawing.Point(137, 106);
+			this.btn6.Name = "btn6";
+			this.btn6.Size = new System.Drawing.Size(50, 50);
+			this.btn6.TabIndex = 9;
+			this.btn6.Text = "6";
+			this.btn6.UseVisualStyleBackColor = true;
+			this.btn6.Click += new System.EventHandler(this.btn6_Click);
+			// 
+			// btn3
+			// 
+			this.btn3.Location = new System.Drawing.Point(137, 162);
+			this.btn3.Name = "btn3";
+			this.btn3.Size = new System.Drawing.Size(50, 50);
+			this.btn3.TabIndex = 12;
+			this.btn3.Text = "3";
+			this.btn3.UseVisualStyleBackColor = true;
+			this.btn3.Click += new System.EventHandler(this.btn3_Click);
+			// 
+			// btn2
+			// 
+			this.btn2.Location = new System.Drawing.Point(81, 162);
+			this.btn2.Name = "btn2";
+			this.btn2.Size = new System.Drawing.Size(50, 50);
+			this.btn2.TabIndex = 11;
+			this.btn2.Text = "2";
+			this.btn2.UseVisualStyleBackColor = true;
+			this.btn2.Click += new System.EventHandler(this.btn2_Click);
+			// 
+			// btn1
+			// 
+			this.btn1.Location = new System.Drawing.Point(25, 162);
+			this.btn1.Name = "btn1";
+			this.btn1.Size = new System.Drawing.Size(50, 50);
+			this.btn1.TabIndex = 10;
+			this.btn1.Text = "1";
+			this.btn1.UseVisualStyleBackColor = true;
+			this.btn1.Click += new System.EventHandler(this.btn1_Click);
+			// 
+			// btn0
+			// 
+			this.btn0.Location = new System.Drawing.Point(81, 218);
+			this.btn0.Name = "btn0";
+			this.btn0.Size = new System.Drawing.Size(50, 50);
+			this.btn0.TabIndex = 13;
+			this.btn0.Text = "0";
+			this.btn0.UseVisualStyleBackColor = true;
+			this.btn0.Click += new System.EventHandler(this.btn0_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 396);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(24, 13);
+			this.label1.TabIndex = 14;
+			this.label1.Text = "0 m";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(752, 395);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(30, 13);
+			this.label2.TabIndex = 15;
+			this.label2.Text = "90 m";
+			// 
+			// delBtn
+			// 
+			this.delBtn.Location = new System.Drawing.Point(137, 218);
+			this.delBtn.Name = "delBtn";
+			this.delBtn.Size = new System.Drawing.Size(50, 50);
+			this.delBtn.TabIndex = 17;
+			this.delBtn.Text = "del";
+			this.delBtn.UseVisualStyleBackColor = true;
+			this.delBtn.Click += new System.EventHandler(this.delBtn_Click);
+			// 
+			// label3
+			// 
+			this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(193, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(595, 395);
+			this.label3.TabIndex = 16;
+			this.label3.Text = "0";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.label3.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.delBtn);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btn0);
+			this.Controls.Add(this.btn3);
+			this.Controls.Add(this.btn2);
+			this.Controls.Add(this.btn1);
+			this.Controls.Add(this.btn6);
+			this.Controls.Add(this.btn5);
+			this.Controls.Add(this.btn4);
+			this.Controls.Add(this.btn9);
+			this.Controls.Add(this.btn8);
+			this.Controls.Add(this.btn7);
+			this.Controls.Add(this.distBtn);
+			this.Controls.Add(this.progressBar1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Button distBtn;
+		private System.Windows.Forms.Button btn7;
+		private System.Windows.Forms.Button btn8;
+		private System.Windows.Forms.Button btn9;
+		private System.Windows.Forms.Button btn4;
+		private System.Windows.Forms.Button btn5;
+		private System.Windows.Forms.Button btn6;
+		private System.Windows.Forms.Button btn3;
+		private System.Windows.Forms.Button btn2;
+		private System.Windows.Forms.Button btn1;
+		private System.Windows.Forms.Button btn0;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button delBtn;
+		private System.Windows.Forms.Label label3;
 	}
 }
 
--- ShootingRange/Form1.cs
+++ ShootingRange/Form1.cs
@@ -10,10 +10,112 @@ using System.Windows.Forms;
 
 namespace ShootingRange
 {
-	public partial class Form1 : Form
-	{
+	public partial class Form1 : Form {
+		public byte dist, currDist;
 		public Form1() {
 			InitializeComponent();
+			progressBar1.Maximum = 90*15;
+			dist = 0;
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			currDist = dist; //Todo: currDist needs to equal readings
+			progressBar1.Value = currDist*15; //Todo: This should constantly update
+			//Todo: Send signal to motor, compare currDist to dist
+			dist = 0;
+		}
+
+		private void btn7_Click(object sender, EventArgs e) {
+			dist = (byte) (dist * 10 + 7);
+			if (dist > 90) {
+				dist = 90;
+			}
+			this.label3.Text = dist + "";
+		}
+
+		private void btn8_Click(object sender, EventArgs e) {
+			dist = (byte)(dist * 10 + 8);
+			if (dist > 90) {
+				dist = 90;
+			}
+			this.label3.Text = dist + "";
+		}
+
+		private void btn9_Click(object sender, EventArgs e) {
+			dist = (byte)(dist * 10 + 9);
+			if (dist > 90) {
+				dist = 90;
+			}
+			this.label3.Text = dist + "";
+		}
+
+		private void btn4_Click(object sender, EventArgs e) {
+			dist = (byte)(dist * 10 + 4);
+			if (dist > 90) {
+				dist = 90;
+			}
+			this.label3.Text = dist + "";
+		}
+
+		private void btn5_Click(object sender, EventArgs e) {
+			dist = (byte)(dist * 10 + 5);
+			if (dist > 90) {
+				dist = 90;
+			}
+			this.label3.Text = dist + "";
+		}
+
+		private void btn6_Click(object sender, EventArgs e) {
+			dist = (byte)(dist * 10 + 6);
+			if (dist > 90) {
+				dist = 90;
+			}
+			this.label3.Text = dist + "";
+		}
+
+		private void btn3_Click(object sender, EventArgs e) {
+			dist = (byte)(dist * 10 + 3);
+			if (dist > 90) {
+				dist = 90;
+			}
+			this.label3.Text = dist + "";
+		}
+
+		private void btn2_Click(object sender, EventArgs e) {
+			dist = (byte)(dist * 10 + 2);
+			if (dist > 90) {
+				dist = 90;
+			}
+			this.label3.Text = dist + "";
+		}
+
+		private void btn1_Click(object sender, EventArgs e) {
+			dist = (byte)(dist * 10 + 1);
+			if (dist > 90) {
+				dist = 90;
+			}
+			this.label3.Text = dist + "";
+		}
+
+		private void btn0_Click(object sender, EventArgs e) {
+			dist = (byte)(dist * 10);
+			if (dist > 90) {
+				dist = 90;
+			}
+			this.label3.Text = dist + "";
+		}
+
+		private void delBtn_Click(object sender, EventArgs e) {
+			dist = (byte) (dist / 10);
+			this.label3.Text = dist + "";
+		}
+
+		private void label3_Click(object sender, EventArgs e) {
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+
 		}
 	}
 }
